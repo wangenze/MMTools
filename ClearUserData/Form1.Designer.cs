68f21f78c9b2528fddaeba24928caa7cfc536703
wangenze <wangenze@buaa.edu.cn>
2016-03-11 14:46:00 +0800

    Init

---
+++ ClearUserData/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ClearUserData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(133, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "继续";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(214, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "请关闭智能主模型客户端后点击“继续”";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(301, 84);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(317, 123);
+            this.MinimumSize = new System.Drawing.Size(317, 123);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "清除用户数据";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ClearUserData/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClearUserData
+{
+    public partial class Form1 : Form
+    {
+        static string data_path = @"..\data";
+        static string exe_path = @"..\MainModel.exe";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo data_info = new DirectoryInfo(data_path);
+            if (!check_file_avaliable(data_info))
+            {
+                MessageBox.Show("数据占用中，请先关闭客户端！");
+            }
+            else if (!delete_files(data_info))
+            {
+                MessageBox.Show("删除失败，用户目录下可能仍有数据残留", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if (MessageBox.Show("数据清除成功！是否重新登录？", "成功", MessageBoxButtons.OKCancel, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1) == DialogResult.OK)
+                {
+                    FileInfo info = new FileInfo(exe_path);
+                    try
+                    {
+                        ProcessStartInfo process = new ProcessStartInfo(info.FullName);
+                        process.WorkingDirectory = info.DirectoryName;
+                        Process.Start(process);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                Application.Exit();
+            }
+        }
+
+        private bool check_file_avaliable(DirectoryInfo info)
+        {
+            if (!info.Exists) return true;
+            foreach (FileInfo file in info.GetFiles())
+            {
+                if (!file.Exists) continue;
+                FileStream fs = null;
+                try
+                {
+                    fs = new FileStream(file.FullName, FileMode.Open, FileAccess.Read, FileShare.None);
+                }
+                catch
+                {
+                    if (fs != null) fs.Close();
+                    return false;
+                }
+                finally
+                {
+                    if (fs != null) fs.Close();
+                }
+            }
+            foreach (DirectoryInfo dir in info.GetDirectories())
+            {
+                if (!check_file_avaliable(dir)) return false;
+            }
+            return true;
+        }
+
+        private bool delete_files(DirectoryInfo info)
+        {
+            if (!info.Exists) return true;
+            foreach (FileInfo file in info.GetFiles())
+            {
+                if (!file.Exists) continue;
+                try
+                {
+                    File.Delete(file.FullName);
+                }
+                catch { return false; }
+            }
+            foreach (DirectoryInfo dir in info.GetDirectories())
+            {
+                if (dir.Name.ToLower() == "icon") continue;
+                if (!delete_files(dir)) return false;
+                if (dir.GetDirectories().Length == 0)
+                {
+                    try
+                    {
+                        Directory.Delete(dir.FullName);
+                    }
+                    catch { return false; }
+                }
+            }
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
